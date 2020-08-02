3663d2c44bf7b77bc007845e7986d5d63aa39e64
gipark <gipark1993@gmail.com>
2020-08-02 22:47:53 +0900

    ssr 적용 완료

--- back/models/index.js
+++ back/models/index.js
@@ -1,4 +1,10 @@
 const Sequelize = require('sequelize');
+const comment = require('./comment');
+const hashtag = require('./hashtag');
+const image = require('./image');
+const post = require('./post');
+const user = require('./user');
+
 // 환경변수를 env에 담는다(기본값: 'development')
 const env = process.env.NODE_ENV || 'development';
 // config.json에서 환경변수에 맞는 정보를 가져온다.
@@ -8,11 +14,20 @@ const db = {};
 // sequelize가 node와 mysql을 연결해줌
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-db.User = require('./user')(sequelize, Sequelize);
-db.Post = require('./post')(sequelize, Sequelize);
-db.Comment = require('./comment')(sequelize, Sequelize);
-db.Image = require('./image')(sequelize, Sequelize);
-db.Hashtag = require('./hashtag')(sequelize, Sequelize);
+db.Comment = comment;
+db.Hashtag = hashtag;
+db.Image = image;
+db.Post = post;
+db.User = user;
+// db.Comment = require('./comment')(sequelize, Sequelize);
+// db.User = require('./user')(sequelize, Sequelize);
+// db.Post = require('./post')(sequelize, Sequelize);
+// db.Image = require('./image')(sequelize, Sequelize);
+// db.Hashtag = require('./hashtag')(sequelize, Sequelize);
+
+Object.keys(db).forEach(modelName => {
+  db[modelName].init(sequelize);
+})
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -10,12 +10,16 @@ import {
 import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Link from 'next/link';
+import moment from 'moment';
 
 import PostImages from './PostImages';
 import CommentForm from './CommentForm';
 import PostCardContent from './PostCardContent';
 import { REMOVE_POST_REQUEST, LIKE_POST_REQUEST, UNLIKE_POST_REQUEST, RETWEET_REQUEST } from '../reducers/post';
 import FollowButton from './FollowButton';
+
+moment.locale('ko');
 
 const CardWrapper = styled.div`
   margin-bottom: 20px;
@@ -110,19 +114,31 @@ const PostCard = ({ post }) => {
             <Card
               cover={post.Retweet.Images[0] && <PostImages images={post.Retweet.Images} />}
             >
+              <div style={{ float: 'right' }}>{moment(post.createdAt, 'YYYYMMDD').fromNow()}</div>
               <Card.Meta
-                avatar={<Avatar>{post.Retweet.User.nickname[0]}</Avatar>}
+                avatar={(
+                  <Link href={`/user/${post.Retweet.User.id}`}>
+                    <a><Avatar>{post.Retweet.User.nickname[0]}</Avatar></a>
+                  </Link>
+                )}
                 title={post.Retweet.User.nickname}
                 description={<PostCardContent postData={post.Retweet.content} />}
               />
             </Card>
           )
           : (
-            <Card.Meta
-              avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
-              title={post.User.nickname}
-              description={<PostCardContent postData={post.content} />}
-            />
+            <>
+              <div style={{ float: 'right' }}>{moment(post.createdAt, 'YYYYMMDD').fromNow()}</div>
+              <Card.Meta
+                avatar={(
+                  <Link href={`/user/${post.User.id}`}>
+                    <a><Avatar>{post.User.nickname[0]}</Avatar></a>
+                  </Link>
+                )}
+                title={post.User.nickname}
+                description={<PostCardContent postData={post.content} />}
+              />
+            </>
           )}
       </Card>
       {commentFormOpened && (
